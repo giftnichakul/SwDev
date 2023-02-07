29a0092b7e1521b52eccfd9df7ffdce9217c4e8b
Gift <giftnichakul@gmail.com>
2023-02-07 21:14:21 +0700

    Activity 5

--- server.js
+++ server.js
@@ -1,40 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+//Load env vars
+dotenv.config({path: './config/config.env'});
+
+//Connect to database
+connectDB();
 
 //Route files
 const hospitals = require('./routes/hospital');
 
-dotenv.config({path: './config/config.env'});
-
 const app = express();
-
-// app.get('/', (req, res) =>{
-//     res.send('<h1>Hello from express</h1>');
-//     res.status(200).json({success: true, data:{id:1}});
-// })
-
-/*
-app.get('/api/v1/hospitals', (req, res) =>{
-    res.status(200).json({success: true, msg:"Show all hospitals"});
-})
-
-app.get('/api/v1/hospitals/:id', (req, res) =>{
-    res.status(200).json({success: true, msg:`Show all hospital ${req.params.id}`});
-})
-
-app.post('/api/v1/hospitals', (req, res) =>{
-    res.status(200).json({success: true, msg:`Create new hospitals`});
-})
-
-app.put('/api/v1/hospitals/:id', (req, res) =>{
-    res.status(200).json({success: true, msg:`Update hospital ${req.params.id}`});
-})
-
-app.delete('/api/v1/hospitals/:id', (req, res) =>{
-    res.status(200).json({success: true, msg:`Delete hospital ${req.params.id}`});
-})
-*/
+//Body parser
+app.use(express.json());
 
 app.use('/api/v1/hospitals', hospitals);
 const PORT=process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+process.on('unhandledRejection', (err, promise) =>{
+    console.log(`Error: ${err.message}`);
+    server.close(() =>process.exit(1));
+})
