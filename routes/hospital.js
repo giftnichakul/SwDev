77843cc5fe32f73d7a9efdfd342e1972c7baf047
Gift <giftnichakul@gmail.com>
2023-02-15 16:23:30 +0700

    Authorization

--- routes/hospital.js
+++ routes/hospital.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const {getHospital, getHospitals, createHospital, updateHospitals, deleteHospitals} = require('../controllers/hospital');
+const {protect, authorize} = require('../middleware/auth');
 
-router.route('/').get(getHospitals).post(createHospital);
-router.route('/:id').get(getHospital).put(updateHospitals).delete(deleteHospitals);
+router.route('/').get(getHospitals).post(protect, authorize("admin"), createHospital);
+router.route('/:id').get(getHospital).put(protect, authorize("admin"), updateHospitals).delete(protect,authorize("admin"), deleteHospitals);
 
 module.exports=router;
